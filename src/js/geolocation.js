e35be0ddf2ec52c485db2fe9c09a348001a0838f
MakinFantasy <beingp0z1t1v3@gmail.com>
2023-03-22 17:11:05 +0300

    linted

--- src/js/geolocation.js
+++ src/js/geolocation.js
@@ -1,55 +1,55 @@
 export default class Geolocation {
-    constructor(chatInput) {
-      this.chatInput = chatInput;
-    }
-  
-    setLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => this.handleSuccess(position),
-          (error) => this.handleFailure(error),
-        );
-      } else {
-        this.chatInput.dataset.geoResponse = JSON.stringify({
-          header: 'Ваш браузер не поддерживает геолокацию',
-          message: 'Смените браузер, либо введите местоположение вручную',
-        });
-      }
-    }
-  
-    handleSuccess(position) {
+  constructor(chatInput) {
+    this.chatInput = chatInput;
+  }
+
+  setLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => this.handleSuccess(position),
+        (error) => this.handleFailure(error),
+      );
+    } else {
       this.chatInput.dataset.geoResponse = JSON.stringify({
-        success: true,
-        lat: position.coords.latitude,
-        lon: position.coords.longitude,
+        header: 'Ваш браузер не поддерживает геолокацию',
+        message: 'Смените браузер, либо введите местоположение вручную',
       });
     }
-  
-    handleFailure(error) {
-      const response = {
-        header: '',
-        message: '',
-      };
-  
-      switch (error.code) {
-        case error.PERMISSION_DENIED:
-          response.header = 'Настройками текущего браузера запрещен запрос геолокации';
-          response.message = 'Измените настройки конфиденциальности, либо введите местоположение вручную';
-          break;
-        case error.POSITION_UNAVAILABLE:
-          response.header = 'Информация о вашем местоположении недоступна';
-          response.message = 'Введите местоположение вручную';
-          break;
-        case error.TIMEOUT:
-          response.header = 'Истекло время ожидания запроса вашего местоположения';
-          response.message = 'Нажмите "Отмена" и попробуйте ещё раз, либо введите местоположение вручную';
-          break;
-        default:
-          response.header = 'Произошла неизвестная ошибка';
-          response.message = 'Нажмите "Отмена" и попробуйте ещё раз, либо введите местоположение вручную';
-          break;
-      }
-  
-      this.chatInput.dataset.geoResponse = JSON.stringify(response);
-    }
   }
+
+  handleSuccess(position) {
+    this.chatInput.dataset.geoResponse = JSON.stringify({
+      success: true,
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    });
+  }
+
+  handleFailure(error) {
+    const response = {
+      header: '',
+      message: '',
+    };
+
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        response.header = 'Настройками текущего браузера запрещен запрос геолокации';
+        response.message = 'Измените настройки конфиденциальности, либо введите местоположение вручную';
+        break;
+      case error.POSITION_UNAVAILABLE:
+        response.header = 'Информация о вашем местоположении недоступна';
+        response.message = 'Введите местоположение вручную';
+        break;
+      case error.TIMEOUT:
+        response.header = 'Истекло время ожидания запроса вашего местоположения';
+        response.message = 'Нажмите "Отмена" и попробуйте ещё раз, либо введите местоположение вручную';
+        break;
+      default:
+        response.header = 'Произошла неизвестная ошибка';
+        response.message = 'Нажмите "Отмена" и попробуйте ещё раз, либо введите местоположение вручную';
+        break;
+    }
+
+    this.chatInput.dataset.geoResponse = JSON.stringify(response);
+  }
+}
